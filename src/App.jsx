85b19f9e0ce3e27e2f395eb1036396fbfcdb4386
Dniel K <danielu96@wp.pl>
2023-04-19 18:53:01 +0200

    <ToDo/> added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
-
+import Alert from './Alert'
 import './App.css'
+import ToDo from './ToDo'
 
 function App() {
   
-
   return (
     <div className="App">
+      
      <h1>Redux-Toolkit</h1>
+     <p>It will be something with Toolkit</p>
+   <ToDo/>
     </div>
   )
 }
