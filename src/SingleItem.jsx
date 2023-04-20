9dbb1f6a3dda6cf627b51c5fb3c66eb4b5164541
Dniel K <danielu96@wp.pl>
2023-04-20 22:47:12 +0200

    removeHandler added

--- src/SingleItem.jsx
+++ src/SingleItem.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import { removeTask } from './Slices/taskReducer'
+import { useDispatch } from 'react-redux'
 // import { useSelector } from "react-redux";
 const SingleItem = (task) => {
-
+const dispatch= useDispatch()
+const removeHandler=()=>{
+  dispatch(removeTask(task.id))
+}
   return (
-    <div>
+    <div style={{display:'flex',justifyContent:"space-between"
+    }}>
       {/* <h3>SingleItem</h3> */}
 <p> {task.content}</p>
-<p></p>
+<button onClick={removeHandler}>X</button>
+
     </div>
   )
 }
