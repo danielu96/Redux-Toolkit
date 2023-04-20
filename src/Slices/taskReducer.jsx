08254ab96ffbc5da542bb3afac73dd7dbb8ecb19
Dniel K <danielu96@wp.pl>
2023-04-20 22:03:21 +0200

    AddTask fixed

--- src/Slices/taskReducer.jsx
+++ src/Slices/taskReducer.jsx
@@ -9,53 +9,14 @@ const taskReducer = createSlice ({
   name:'task',
   initialState,
   reducers:{
-    AddTask: (state, { payload: { name, value } }) => {
-       const newTask ={
-        name:name,
-
-       }
-      state[name] = value;
-      state.tasks.push(newTask);
+    AddTask: ( state, action ) => {       
+      state.tasks.push(action.payload)
+      // state.tasks= action.payload.tasks
+       }      
+          
     },
-  //   AddTask: (state, action) => {
-  //     const { id, cena, title, amount, availableProducts, name, price } = action.payload;
-  //     const existingItem = state.cartItems.find((i) => i.id === id)
-  //     if (existingItem) {
-  //         const tempCart = state.cartItems.map((cartItem) => {
-  //             if (cartItem.id === id) {
 
-  //                 let newAmount = cartItem.amount + amount
-  //                 if (newAmount > cartItem.max) {
-  //                     newAmount = cartItem.max
-  //                 }
-
-  //                 return { ...cartItem, amount: newAmount }
-
-  //             } else {
-  //                 return cartItem
-  //             }
-
-  //         })
-  //         state.cartItems = (tempCart)
-  //         // return { state, cartItems: tempCart }
-
-  //     }
-  //     else {
-  //         let max = availableProducts
-  //         const newItem = {
-  //             id: id,
-  //             name,
-  //             price,
-  //             amount,
-  //             availableProducts,
-  //             max,
-  //         }
-  //         state.cartItems.push(newItem);
-  //         // return { ...state, cartItems: [...state.cartItems, newItem] }
-  //     }
-     
-  // },
   }
-})
+)
 export const {AddTask} = taskReducer.actions;
 export default taskReducer.reducer
