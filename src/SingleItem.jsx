8b7a899af73e689dc9f5a4cf856edc4fdb7892a6
Dniel K <danielu96@wp.pl>
2023-04-26 19:27:34 +0200

    cleared

--- src/SingleItem.jsx
+++ src/SingleItem.jsx
@@ -1,32 +1,32 @@
 import React ,{useState,useRef}from 'react'
 import { removeTask,renameTask } from './Slices/taskReducer'
 import { useDispatch } from 'react-redux'
+import Checkbox from "./Checkbox";
 // import { useSelector } from "react-redux";
-const SingleItem = (task,content) => {
+const SingleItem = (task,done,onToggle) => {
+  
 const dispatch= useDispatch()
-const inputRef = useRef()
 const [editMode,setEditMode] = useState(false);
-const removeHandler=()=>{
-  dispatch(removeTask( task.id ))
-}
-  return (
-    <div style={{display:'flex',justifyContent:"space-between"
-    }}>
 
+  return (
+    
+    <div style={{width:'400px',display:'flex',justifyContent:"space-between"
+    }}>    
+<Checkbox checked={done} onClick={() => onToggle(!done)} />
 {!editMode ? (
         <div className="" onClick={() => setEditMode(prev => !prev)}>
           <span>{task.content}</span>
         </div>
       ):
        (
-        <form onSubmit={ev => {ev.preventDefault();setEditMode(false);}}>
+        <form style={{marginLeft:'1rem'}} onSubmit={ev => {ev.preventDefault();setEditMode(false);}}>
           <input type="text" value={task.content}
-                 onChange={()=> dispatch(renameTask()) } />
+                 onChange={(ev)=> dispatch(renameTask(ev.target.value)) } />
                  <button>update</button>
         </form>
       )}   
 
-<button onClick={removeHandler}>X</button>
+<button onClick={()=> dispatch(removeTask(task.id))}>X</button>
     </div>
   )
 }
