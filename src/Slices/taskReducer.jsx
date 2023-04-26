33033fd7de8bd9f6923723337613bf7a5addfe6c
Dniel K <danielu96@wp.pl>
2023-04-26 11:54:31 +0200

    clearTasks added

--- src/Slices/taskReducer.jsx
+++ src/Slices/taskReducer.jsx
@@ -2,6 +2,7 @@ import { createSlice } from '@reduxjs/toolkit'
 import React from 'react'
 
 const initialState ={
+  content:'',
   tasks:[],
 }
 
@@ -10,16 +11,51 @@ const taskReducer = createSlice ({
   initialState,
   reducers:{
     AddTask: ( state, action ) => {       
-      state.tasks.push(action.payload)
-      // state.tasks= action.payload.tasks
-       }  ,
-       removeTask:(state,action)  =>{
+      state.tasks.push(action.payload)  
+      // const newTask = {
+      //   // id: nanoid(),
+      //   id:Math.random()* 1000,
+      //   content: action.payload.task,
+      },
+      // state.tasks.push(newTask);   
+      //  },
+    removeTask:(state,action)  =>{
        state.tasks =  state.tasks.filter((task) => task.id !== action.payload);
-       } 
-          
+       } ,
+       renameTask:(state,action)  =>{
+        console.log(action)
+// state.tasks= state.tasks.map(i=> i.id=== action.payload.id ? action.payload :i)
+// state.tasks = state.tasks.filter(task => task.id === action.payload);
+// return state
+
+  //  state.tasks = state.tasks.filter(task => task.id === action.payload);
+  //  const index = state.tasks.findIndex((todo) => todo.id === action.payload.id);
+  //  state[index].content = action.payload.content;
+        //  let content = action.payload.content;
+
+        // state.tasks =  state.tasks.filter((task) => task.id === action.payload);
+
+
+
+            //  const index = state.tasks.findIndex(task => task.id === action.payload.id);
+
+        // state.tasks =  state.tasks.find((task) => task.id === action.payload);
+          // const index = state.tasks.filter(task => task.id === action.payload.id);
+        
+        // state.tasks= state.filter((task) => 
+        // task.id === action.payload.id ? action.payload: task
+        // );
+        // state.tasks =  state.tasks.filter((task) => task.id === action.payload);
+        // const index = state.tasks.filter(task => task.id === action.payload.id);
+        // const updatedState = [...state];
+        // updatedState[index].content = action.payload.contemt;
+},
+    clearTasks :(state)=> {
+    state.tasks=[]  
+  }   
     },
 
   }
 )
-export const {AddTask,removeTask} = taskReducer.actions;
+export const {AddTask,removeTask,renameTask,clearTasks} = taskReducer.actions;
 export default taskReducer.reducer
