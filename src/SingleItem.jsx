06d66c304ef0d3c9d6bb3b92bd1ae7951cc7a2fc
Dniel K <danielu96@wp.pl>
2023-04-26 11:54:54 +0200

    renameTask added

--- src/SingleItem.jsx
+++ src/SingleItem.jsx
@@ -1,21 +1,33 @@
-import React from 'react'
-import { removeTask } from './Slices/taskReducer'
+import React ,{useState,useRef}from 'react'
+import { removeTask,renameTask } from './Slices/taskReducer'
 import { useDispatch } from 'react-redux'
 // import { useSelector } from "react-redux";
-const SingleItem = (task) => {
+const SingleItem = (task,content) => {
 const dispatch= useDispatch()
+const inputRef = useRef()
+const [editMode,setEditMode] = useState(false);
 const removeHandler=()=>{
-  dispatch(removeTask(task.id))
+  dispatch(removeTask( task.id ))
 }
   return (
     <div style={{display:'flex',justifyContent:"space-between"
     }}>
-      {/* <h3>SingleItem</h3> */}
-<p> {task.content}</p>
-<button onClick={removeHandler}>X</button>
 
+{!editMode ? (
+        <div className="" onClick={() => setEditMode(prev => !prev)}>
+          <span>{task.content}</span>
+        </div>
+      ):
+       (
+        <form onSubmit={ev => {ev.preventDefault();setEditMode(false);}}>
+          <input type="text" value={task.content}
+                 onChange={()=> dispatch(renameTask()) } />
+                 <button>update</button>
+        </form>
+      )}   
+
+<button onClick={removeHandler}>X</button>
     </div>
   )
 }
-
 export default SingleItem
