a6c6702065082b8536c37e823e63520a65ed75b9
Dniel K <danielu96@wp.pl>
2023-05-08 21:31:07 +0200

    new update added

--- src/Slices/taskReducer.jsx
+++ src/Slices/taskReducer.jsx
@@ -7,22 +7,30 @@ const initialState ={
   // isEditing:false,
   
   tasks:items,
+  
 }
 
 const taskReducer = createSlice ({
   name:'task',
   initialState,
   reducers:{
-    AddTask: ( state, action ) => {       
+    AddTask: ( state, action ) => {   
+//       const newTask = {
+//         // id: nanoid(),
+//         id:Date.now(),
+//         // id:Math.random()* 1000,
+//         title: action.payload,
+//         completed:false,
+//       }
+//  state.tasks.push(newTask)  ;
       state.tasks.push(action.payload)  
-      // const newTask = {
-      //   // id: nanoid(),
-      //   id:Math.random()* 1000,
-      //   content: action.payload.task,
+     
       localStorage.setItem('tasks', JSON.stringify(state.tasks.map
         (item => item)))
       },
       UpdateTask:(state,action)=>{
+        state.name=action.payload.name,
+  state.value=action.payload.value
         let data = action.payload;
         const UpdateA=[];
         state.tasks.map((task)=>{
@@ -54,12 +62,53 @@ const taskReducer = createSlice ({
         localStorage.setItem('tasks', JSON.stringify(state.tasks.map
           (item => item)))
       },
+// update (state,action){
+//   const task = state.tasks.find(task => task.id === action.payload.id);
+//   if(task === !task){
+//  title=action.payload,
+//   state.value=action.payload.value
+//   }
+//   localStorage.setItem('tasks', JSON.stringify(state.tasks.map
+//     (item => item)))
+
+// },
+update: (state, action) => {
+const tasks =  localStorage.setItem('tasks', JSON.stringify(state.tasks.map
+        (item => item)))
+  // const tasks = window.localStorage.getItem('tasks');
+  if (tasks) {
+    const tasksListArr = JSON.parse(tasks);
+    tasksListArr.forEach((task) => {
+      if (task.id === action.payload.id) {
+        // task.status = action.payload.status;
+        task.title = action.payload.title;
+      }
+    });
+    // window.localStorage.setItem('todoList', JSON.stringify(tasksListArr));
+    state.tasks = [...tasksListArr];
+  }
+},
+
       changeTask(state, action) {
-        // const task = state.tasks.find(task => task.id === action.payload)
-        const name= action.payload
+        const task= action.payload
+        // const name= action.payload
+         state.tasks.find(task => task.id === action.payload)
+      //  state.name = action.payload.name ;
+       state.value = action.payload.value ;
+
+        //  state.tasks.map(task => task.id === action.payload.id ? action.payload : task)
+        // const name= e.target.name;
+    
+       
         if (task) {
-          task[name] = task.name
+          state[task.name] = task.name
         }
+        // if (value) {
+        //   state[value] = value
+        // }
+        console.log(action.payload)
+      return  action.payload,
+       
         localStorage.setItem('tasks', JSON.stringify(state.tasks.map
           (item => item)))
       },
@@ -84,9 +133,12 @@ const taskReducer = createSlice ({
         (item => item)))
        } ,
        renameTask:(state,action)  =>{
-        const task = state.tasks.find(task => task.id === action.payload);
-        state[task.name]= task.name;
-        console.log(action)
+        const task = state.tasks.find(task => task.id === action.payload.id);
+        // state[task.name]= task.name;
+         task.name = action.payload.name
+         task.value = action.payload.value
+        console.log(task.name)
+        task.name;
         localStorage.setItem('tasks', JSON.stringify(state.tasks.map
           (item => item)))
 // state.tasks= state.tasks.map(i=> i.id=== action.payload.id ? action.payload :i)
@@ -124,5 +176,6 @@ const taskReducer = createSlice ({
 
   }
 )
-export const {AddTask,removeTask,renameTask,clearTasks,handleChange,setEditTask,ToggleTask,changeTask,UpdateTask} = taskReducer.actions;
+export const {AddTask,removeTask,renameTask,clearTasks,
+  handleChange,setEditTask,ToggleTask,changeTask,UpdateTask,update} = taskReducer.actions;
 export default taskReducer.reducer
