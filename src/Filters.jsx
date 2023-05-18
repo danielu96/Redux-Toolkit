2f2461a890bc4950d20f763a0fc768c685a0325e
Dniel K <danielu96@wp.pl>
2023-05-18 14:00:20 +0200

    Filters with style added

---
+++ src/Filters.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Filters = () => {
+  return (
+    <div style={{display:'flex',marginTop:'1rem',justifyContent:'space-between',width:'200px',marginRight:'auto',marginLeft:'auto'}}>
+        Filters
+    <button>All</button>
+    <button>done</button>
+    <button>undone</button>
+    </div>
+  )
+}
+
+export default Filters
