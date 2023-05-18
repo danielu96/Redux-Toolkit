3e42e80978873dcd1453996d84d64d89cca37c44
Dniel K <danielu96@wp.pl>
2023-05-18 19:34:41 +0200

    clear

--- src/Filters.jsx
+++ src/Filters.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import { setFilter } from './Slices/taskReducer'
+import { useDispatch } from 'react-redux'
 
 const Filters = () => {
+    const dispatch= useDispatch()
   return (
     <div style={{display:'flex',marginTop:'1rem',justifyContent:'space-between',width:'200px',marginRight:'auto',marginLeft:'auto'}}>
         Filters
-    <button>All</button>
-    <button>done</button>
-    <button>undone</button>
+    <button onClick={()=>dispatch(setFilter('all'))}>All</button>
+    <button onClick={()=>dispatch(setFilter('done'))}>done</button>
+    <button onClick={()=>dispatch(setFilter('undone'))}>undone</button>
     </div>
   )
 }
