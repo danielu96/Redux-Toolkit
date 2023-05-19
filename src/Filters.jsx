62c7056dbf0c2de946771d875faf2fcc08984298
Dniel K <danielu96@wp.pl>
2023-05-19 13:15:10 +0200

    clear

--- src/Filters.jsx
+++ src/Filters.jsx
@@ -4,12 +4,14 @@ import { useDispatch } from 'react-redux'
 
 const Filters = () => {
     const dispatch= useDispatch()
+
   return (
     <div style={{display:'flex',marginTop:'1rem',justifyContent:'space-between',width:'200px',marginRight:'auto',marginLeft:'auto'}}>
         Filters
     <button onClick={()=>dispatch(setFilter('all'))}>All</button>
     <button onClick={()=>dispatch(setFilter('done'))}>done</button>
     <button onClick={()=>dispatch(setFilter('undone'))}>undone</button>
+    
     </div>
   )
 }
