d587dff57e8debaa24f221f7c92bf97d122d3153
Dniel K <danielu96@wp.pl>
2023-05-23 17:26:38 +0200

    handleDropdownChange fixed

--- src/SingleItem.jsx
+++ src/SingleItem.jsx
@@ -1,66 +1,83 @@
 import React ,{useState,useEffect}from 'react'
-import { removeTask,setEditTask,ToggleTask,update,changeTask,Toggler, setFilter,UpdateTask } from './Slices/taskReducer'
+import { removeTask,UpdateStatus,ToggleTask,update,changeTask,Toggler, setFilter,UpdateTask, GetTask } from './Slices/taskReducer'
 import { useDispatch, useSelector } from 'react-redux'
 import Checkbox from "./Checkbox";
 
+// const initialState={
+//   status:""
+// }
+ 
+
 const SingleItem = (task) => {
+  // const [state, setState] = useState(initialState);
+ 
   // const[title,setTitle] = useState('');
 const {completed,id,time,title,status}=task
 
+
+
+    // useEffect(()=>{ 
+    // if(task) {  
+    //    completed:false = task.status==='undone',
+    //    completed:true = task.status==='done',
+    //   })
+    // }
+    
+    
+  // },[task,completed]);  
   //   useEffect(()=>{ 
-  //   if(task) {  
-  //     dispatch(update(title))
-  //   }
-  // },[task]);
+     
+  //     dispatch(GetTask(id))    
+  // },[id]); 
 
 
-
-// useEffect(() => {
-//         // dispatch(SortProducts(products))
-//         dispatch(setEditTask(task.name))
-//         console.log(task.name)
-//     }, [task.name
-//         // products.sort,
-//         // products.filters
-//     ])
-
-
-  
 const dispatch= useDispatch();
 const [ newTitle,setNewTitle]=useState();
 const [editMode,setEditMode] = useState(false);
+// const [status,setStatus]= useState('undone')
 const removeHandler=()=>{  
   dispatch(removeTask(id))
-  // showAlert(true, 'danger', 'you deleted one value')
 }
-// handleUpdate = (e) =>{
-//   e.preventDefault();
-//   dispatch(update({name,value}))
-// };
+const handleDropdownChange = (e) => {
+  // const { name, value } = e.target;
+  // const {status,name}=e.target
+  // setState({ ...state, [name]: value });
+  dispatch(UpdateStatus({
+    // ...task, 
+    id,
+    name:e.target.value,    
+    status:e.target.value,
+    // completed:e.target.value
+  }
 
-const renameHandler=(e)=>{
-  //  const name = e.target.name;
+  ))
+  dispatch(ToggleTask(id))
+};
+
+
+const renameHandler=(e)=>{  
   const title = e.target.value;
   dispatch(update({
-    ...task,
-    // name:task.name,
-    title,
-    // title:(`${title}`),
-    // time:time,
-    // value:task.value
+    ...task,    
+    title, 
+    status  
   }))
-  //    useEffect(()=>{ 
-  //   if(task) {  
-  //     dispatch(update(title))
-  //   }
-  // },[task]);
+
+//      useEffect(()=>{ 
+//     if(editMode && task) {  
+//       title(task.title);
+//       setStatus(task.status)
+//     }
+//   },[task,editMode]);
+// } ;
+// useEffect(()=>{ 
+//   if(!completed && task) {  
+    
+//     dispatch(UpdateStatus(task.status))
+//     // dispatch(UpdateTask(task.title))
+//   }
+// },[task,completed]);
 } 
-
-// handleInputChange = (e)=>{
-// const {name,value}=e.target;
-// setState({...state,[name]:value})
-// }
-
 //  const updateTaskDone=(id, newCompleted) =>{
 //     setTasks(prev => {
 //       const newTasks = [...prev];
@@ -72,25 +89,53 @@ const renameHandler=(e)=>{
 // const onToggleTask= () =>{completed => dispatch(ToggleTask(id, completed))} 
   return (
     <>
+    
     <div className='singleItem' >  
-     <input type='checkbox' checked={completed}
-  // onChange={()=>onToggle(task.id)}
+     <input type='checkbox' checked={completed}  
   onChange=
-  { ()=>dispatch(ToggleTask(id))   }
+  { 
+    // ()=>dispatch(ToggleTask(id)) 
+    // ||
+     handleDropdownChange  }
   ></input> 
+ 
    <p>{time}</p>
-   <p 
+   {/* <p 
   //  onClick={  () =>   dispatch(setFilter('done'))  } 
-  onClick={  () =>   dispatch(Toggler('done'))  } 
-   >{status}</p>
+  onClick={  () =>   dispatch(Toggler({id:task.id,status:task.status}))  } 
+   >{status}</p> */}
+    <select style={{margin:'1rem'}} 
+      name="status"
+      onChange={handleDropdownChange}
+    // onChange=
+    // {  () =>  
+    //   dispatch(
+    //     UpdateStatus({
+    //   id:task.id,
+    //   status:task.status})) 
+    //  } 
+    // // {()=> setStatus({id:task.id,status:task.status})}
+    >    <option>select status</option>
+            <option 
+            
+            value="undone" select={status === "undone" ? status : ""}
+            >
+            undone
+        </option>
+        <option  
+        
+          value="done" select={status === "done" ? status : ""}
+        >
+          done
+           {/* undone */}
+            </option>
+      </select>
  {/* <Checkbox checked={!done} onClick={() => onToggle(task.id)} /> */}
  <Checkbox checked={completed} 
- onClick={  () =>   dispatch(ToggleTask(id))  }  />    
-   
+ onClick={  () =>   dispatch(ToggleTask(id))  }  />      
 {!editMode ? (
-        <div className='singleTask' onClick={() => setEditMode(prev => !prev)}>
-         
-          <p  style={completed===true?{textDecoration:'line-through'}:{textDecoration:'none'}}>
+        <div className='singleTask' onClick={() => setEditMode(prev => !prev)}>         
+          <p  style={ completed===true || task.status==='done' ?{textDecoration:'line-through'}:{textDecoration:'none'}}>
                 {title}                
                 </p>               
                 <button style={{height:'2rem',marginTop:'1rem'}}>edit</button>
@@ -104,8 +149,7 @@ const renameHandler=(e)=>{
           // value={title}
          onChange= 
           // {renameHandler}
-        //  { (e)=>dispatch(update(e.target.value))}
-          //  {(e)=>dispatch(update(e.target.value))}         
+        //  { (e)=>dispatch(update(e.target.value))}                  
                 {(e)=>setNewTitle(e.target.value)}    
                        />  
                        {/* <button  onClick= { ()=>dispatch(update(task.title))} >update</button>      */}
