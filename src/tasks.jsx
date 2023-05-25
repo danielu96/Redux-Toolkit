c19d33c135f65fc5767f51c8de4b9cbf1c18e5a3
Dniel K <danielu96@wp.pl>
2023-05-25 21:14:14 +0200

    create tasks.jsx

---
+++ src/tasks.jsx
@@ -0,0 +1,32 @@
+const tasks=[
+    {
+        "id":"0",
+        "time":new Date().toLocaleString(),
+        "title":"uczyć",
+        "status":"undone"
+
+    },
+    {
+        "id":"1",
+        "time":new Date().toLocaleString(),
+        "title":"napisać",
+        "status":"done"
+
+    },
+    {
+        "id":"2",
+        "time":new Date().toLocaleString(),
+        "title":"posprzątać",
+        "status":"not slected"
+
+    },
+    {
+        "id":"3",
+        "time":new Date().toLocaleString(),
+        "title":"pozamiatać",
+        "status":"done"
+
+    }
+
+]
+export default tasks;
