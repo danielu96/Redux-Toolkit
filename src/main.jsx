76e18732962b32651141bdbe0a895a3d98afbf45
Dniel K <danielu96@wp.pl>
2023-06-02 17:39:06 +0200

    import bootstrap disabled

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App'
 import './index.css'
 import  store  from './store';
 import { Provider } from 'react-redux';
+//  import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
