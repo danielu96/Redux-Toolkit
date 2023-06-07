da338637ed4acdce049b6b161ddda08eb718582d
Dniel K <danielu96@wp.pl>
2023-06-07 17:28:14 +0200

    darkMode:modeReducer added

--- src/store.jsx
+++ src/store.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { configureStore, createReducer } from '@reduxjs/toolkit'
 import taskReducer from './Slices/taskReducer';
 
+  import modeReducer from './Slices/modeReducer';
+
 const store = configureStore({
   reducer: {
     task: taskReducer,
- 
+    darkMode:modeReducer
 
   },
 });
