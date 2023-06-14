ccfaeffe473bedffcd3ccf95bc12aa1bc5e50331
Dniel K <danielu96@wp.pl>
2023-06-14 12:07:34 +0200

    className='my-btn' to buttons added

--- src/Filters.jsx
+++ src/Filters.jsx
@@ -9,9 +9,9 @@ const Filters = () => {
   return (
     <div style={{display:'flex',marginTop:'1rem',justifyContent:'space-between',width:'200px',marginRight:'auto',marginLeft:'auto'}}>
         Filters
-    <button onClick={()=>dispatch(setFilter('All'))}>All</button>
-    <button onClick={()=>dispatch(setFilter('done'))}>done</button>
-    <button onClick={()=>dispatch(setFilter('undone'))}>undone</button>
+    <button className='my-btn' onClick={()=>dispatch(setFilter('All'))}>All</button>
+    <button className='my-btn' onClick={()=>dispatch(setFilter('done'))}>done</button>
+    <button className='my-btn' onClick={()=>dispatch(setFilter('undone'))}>undone</button>
    
     
     </div>
